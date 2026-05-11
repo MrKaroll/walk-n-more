8dc13a699544faf257e81215f77f65a98c6e8305
MrKaroll <karol.kink@gmail.com>
2026-05-11 16:59:20 +0300

    added native bridge files

---
+++ android/app/src/main/java/com/mrkaroll/steptracker/Stepmodule.kt
@@ -0,0 +1,19 @@
+package com.mrkaroll.steptracker
+
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+
+class StepModule(reactContext: ReactApplicationContext) :
+    ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "StepModule"
+    }
+
+    @ReactMethod
+    fun getTodaySteps(promise: Promise) {
+        promise.resolve(1234)
+    }
+}
